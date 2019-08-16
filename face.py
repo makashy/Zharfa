ee26f2274d50c6d05ab05a807e49e8202163783c
Amin <m.amin.kashi@gmail.com>
2019-08-16 16:20:48 +0430

    Using imagetools package for handling input images

--- face.py
+++ face.py
@@ -4,16 +4,15 @@ from kivy.clock import Clock
 # from kivy.uix.widget import Widget
 # import time
 from kivy.config import Config
-from kivy.graphics.texture import Texture
 from kivy.uix.boxlayout import BoxLayout
 # import pandas as pd
 # import numpy as np
 from kivy.uix.recycleview import RecycleView
 
-import cv2
 from utils.databasetools import DataBase
 # import utils.debugtools
 from utils.facetools import WatchDog
+from utils.imagetools import InputImage
 
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 Config.set('kivy', 'window_icon', './images/facelogo010-01.png')
@@ -59,10 +58,8 @@ class ZharfaApp(App):
                             predictor_path=PREDICTOR_PATH,
                             recognizer_path=RECOGNIZER_PATH)
 
-        self.capture = cv2.VideoCapture(0)
-        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920 / 2)
-        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080 / 2)
-        # cv2.namedWindow("CV2 Image")
+        self.input_image = InputImage('images/facelogo010-01.png')
+        self.input_image.change_source(0)
 
         return self.cm
 
@@ -76,11 +73,8 @@ class ZharfaApp(App):
         # pass
         if self.cm.ids['play'].state is 'down':
             # 1.capture a frame
-            for _ in range(10):
-                ret, frame = self.capture.read()
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # 2.detecting faces
-            # dets = self.dog.detector(frame)
+            frame = self.input_image.get_frame()
+
             result_list = self.dog.identify(frame)
             if result_list is not None:
                 correspondence_dict = self.db.update(result_list)
@@ -88,26 +82,21 @@ class ZharfaApp(App):
                 for i in range(len(result_list['DetectedFaces'])):
                     rect = result_list['DetectedFaces'][i].rect
                     # emb = result_list['RecognitionID'][i]
-                    cv2.rectangle(frame, (rect.right(), rect.top()),
-                                  (rect.left(), rect.bottom()), (0, 255, 0), 3)
+                    frame = self.input_image.add_face_boxes(frame, rect)
+
                     id_num = correspondence_dict[i]
                     text = self.db.dataframe.at[
                         id_num, 'FirstName'] + ' ' + self.db.dataframe.at[
                             id_num, 'LastName']
-                    font = cv2.FONT_HERSHEY_SIMPLEX
-                    cv2.putText(frame, text, (rect.left(), rect.bottom() + 10),
-                                font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                    
+                    frame = self.input_image.add_name(frame, text, rect)
 
             # cv2.imshow("CV2 Image", frame)
             # convert it to texture
-            buf1 = cv2.flip(frame, 0)
-            buf = buf1.tostring()
-            texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]),
-                                      colorfmt='bgr')
-            texture1.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
-            # display image from the texture
+
+            texture = self.input_image.get_kivy_texture(frame)
             self.cm.ids['camera'].color = (1, 1, 1, 1)
-            self.cm.ids['camera'].texture = texture1
+            self.cm.ids['camera'].texture = texture
 
 
 if __name__ == '__main__':
