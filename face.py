0bbd2406786d46c7fbf2bb1485ddb63b2612a706
Amin <m.amin.kashi@gmail.com>
2019-08-16 14:35:20 +0430

    Clean-up

--- face.py
+++ face.py
@@ -1,37 +1,40 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.widget import Widget
-import time
-from kivy.config import Config
 # from skimage.io import imsave
 from kivy.clock import Clock
+# from kivy.uix.widget import Widget
+# import time
+from kivy.config import Config
 from kivy.graphics.texture import Texture
-import cv2
-import pandas as pd
-import numpy as np
+from kivy.uix.boxlayout import BoxLayout
+# import pandas as pd
+# import numpy as np
 from kivy.uix.recycleview import RecycleView
-# import utils.debugtools
-from utils.facetools import *
+
+import cv2
 from utils.databasetools import DataBase
+# import utils.debugtools
+from utils.facetools import WatchDog
 
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
-Config.set('kivy','window_icon','./images/facelogo010-01.png')
-Config.set('graphics','minimum_width',425)
-Config.set('graphics','minimum_height',200)
+Config.set('kivy', 'window_icon', './images/facelogo010-01.png')
+Config.set('graphics', 'minimum_width', 425)
+Config.set('graphics', 'minimum_height', 200)
 
-detector_path = "./utils/core/mmod_human_face_detector.dat"
-predictor_path = "./utils/core/shape_predictor_5_face_landmarks.dat"
+
+DETECTOR_PATH = "./utils/core/mmod_human_face_detector.dat"
+PREDICTOR_PATH = "./utils/core/shape_predictor_5_face_landmarks.dat"
 # recognizer_path = "./utils/core/dlib_face_recognition_resnet_model_v1.dat"
-recognizer_path = './utils/core/ms_celeb_1M_facenet_keras_weights.h5'
-database_address = './database/'
+RECOGNIZER_PATH = './utils/core/ms_celeb_1M_facenet_keras_weights.h5'
+DATABASE_ADDRESS = './database/'
+
 
 class RV(RecycleView):
     def __init__(self, **kwargs):
         super(RV, self).__init__(**kwargs)
         self.data = [{'text': str(x)} for x in range(100)]
 
-# class DataCard(RelativeLayout):
 
+# class DataCard(RelativeLayout):
 
 
 class CameraClick(BoxLayout):
@@ -40,46 +43,30 @@ class CameraClick(BoxLayout):
         Function to capture the images and give them the names
         according to their captured time and date.
         '''
-        camera = self.ids['camera']
+        # camera = self.ids['camera']
         # timestr = time.strftime("%Y%m%d_%H%M%S")
         # camera.export_to_png("IMG_{}.png".format(timestr))
         print("Captured")
 
 
-
 class ZharfaApp(App):
-
     def build(self):
         self.cm = CameraClick()
-        self.db = DataBase(database_address)
-        Clock.schedule_interval(self.update, 1.0/33.0)
+        self.db = DataBase(DATABASE_ADDRESS)
+        Clock.schedule_interval(self.update, 1.0 / 33.0)
 
-        self.dog = WatchDog(detector_path=detector_path, 
-                            predictor_path=predictor_path, 
-                            recognizer_path=recognizer_path)
+        self.dog = WatchDog(detector_path=DETECTOR_PATH,
+                            predictor_path=PREDICTOR_PATH,
+                            recognizer_path=RECOGNIZER_PATH)
 
-        self.capture = cv2.VideoCapture(1)
-        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH,1920/2)
-        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT,1080/2)
+        self.capture = cv2.VideoCapture(0)
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920 / 2)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080 / 2)
         # cv2.namedWindow("CV2 Image")
-        
+
         return self.cm
 
     # def show_people_info(self, result_list):
-    #     for i in range(len(result_list['detected_people'])): 
-    #         rect = result_list['detected_people'][i].rect
-    #         emb = result_list['embeddings'][i]
-    #         cv2.rectangle(frame,(rect.right(),rect.top()),(rect.left(),rect.bottom()),(0,255,0),3)
-            
-    #         print('##########################' + str(i))
-    #         print(emb)
-    #         text = "unknown " + str( compare2(dbase.RecognitionID[0], emb))
-    #         for index in dbase.index:
-    #             if compare(dbase.RecognitionID[index], emb):
-    #                 text = dbase.FirstName[index] + ' ' + dbase.LastName[index] + ' ' + str( compare2(dbase.RecognitionID[0], emb))
-    
-    #         font = cv2.FONT_HERSHEY_SIMPLEX
-    #         cv2.putText(frame,text,(rect.left(),rect.bottom()+10), font, 1,(255,255,255),2,cv2.LINE_AA)
     #     #show info of new people
     #         # if a person is in the view , reset his removal timer
 
@@ -87,39 +74,41 @@ class ZharfaApp(App):
 
     def update(self, dt):
         # pass
-        if(self.cm.ids['play'].state is 'down'):
+        if self.cm.ids['play'].state is 'down':
             # 1.capture a frame
-            ret, frame = self.capture.read()
+            for _ in range(10):
+                ret, frame = self.capture.read()
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             # 2.detecting faces
             # dets = self.dog.detector(frame)
             result_list = self.dog.identify(frame)
-            if result_list is not None:  
+            if result_list is not None:
                 correspondence_dict = self.db.update(result_list)
                 self.db.save_data_frame()
-                for i in range(len(result_list['DetectedFaces'])): 
+                for i in range(len(result_list['DetectedFaces'])):
                     rect = result_list['DetectedFaces'][i].rect
-                    emb = result_list['RecognitionID'][i]
-                    cv2.rectangle(frame,(rect.right(),rect.top()),(rect.left(),rect.bottom()),(0,255,0),3)
-                    
-                    # text = "unknown " + str( compare2(dbase.RecognitionID[0], emb))
-                    # for index in dbase.index:
-                    #     if compare(dbase.RecognitionID[index], emb):
-                    #         text = dbase.FirstName[index] + ' ' + dbase.LastName[index] + ' ' + str( compare2(dbase.RecognitionID[0], emb))
+                    # emb = result_list['RecognitionID'][i]
+                    cv2.rectangle(frame, (rect.right(), rect.top()),
+                                  (rect.left(), rect.bottom()), (0, 255, 0), 3)
                     id_num = correspondence_dict[i]
-                    text = self.db.dataframe.at[id_num, 'FirstName'] + ' ' + self.db.dataframe.at[id_num, 'LastName']
+                    text = self.db.dataframe.at[
+                        id_num, 'FirstName'] + ' ' + self.db.dataframe.at[
+                            id_num, 'LastName']
                     font = cv2.FONT_HERSHEY_SIMPLEX
-                    cv2.putText(frame,text,(rect.left(),rect.bottom()+10), font, 1,(255,255,255),2,cv2.LINE_AA)
+                    cv2.putText(frame, text, (rect.left(), rect.bottom() + 10),
+                                font, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
             # cv2.imshow("CV2 Image", frame)
             # convert it to texture
             buf1 = cv2.flip(frame, 0)
             buf = buf1.tostring()
-            texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]),
+                                      colorfmt='bgr')
             texture1.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
             # display image from the texture
-            self.cm.ids['camera'].color = (1,1,1,1)
+            self.cm.ids['camera'].color = (1, 1, 1, 1)
             self.cm.ids['camera'].texture = texture1
+
 
 if __name__ == '__main__':
     ZharfaApp().run()
--- utils/facetools.py
+++ utils/facetools.py
@@ -1,10 +1,10 @@
-
 import numpy as np
 import dlib
 import pandas as pd
 from utils.inception_resnet_v1 import InceptionResNetV1
 
 pd.set_option('mode.chained_assignment', 'raise')
+
 
 def prewhiten(x):
     if x.ndim == 4:
@@ -18,66 +18,75 @@ def prewhiten(x):
 
     mean = np.mean(x, axis=axis, keepdims=True)
     std = np.std(x, axis=axis, keepdims=True)
-    std_adj = np.maximum(std, 1.0/np.sqrt(size))
+    std_adj = np.maximum(std, 1.0 / np.sqrt(size))
     y = (x - mean) / std_adj
     return y
 
+
 def l2_normalize(x, axis=-1, epsilon=1e-10):
-    output = x / np.sqrt(np.maximum(np.sum(np.square(x), axis=axis, keepdims=True), epsilon))
+    output = x / np.sqrt(
+        np.maximum(np.sum(np.square(x), axis=axis, keepdims=True), epsilon))
     return output
+
 
 def compare(vec1, vec2):
     return np.linalg.norm(vec1 - vec2) < 0.55
 
+
 def compare2(vec1, vec2):
     return np.linalg.norm(vec1 - vec2)
+
 
 # def identify(face_chips, database):
 #     embedings = model.predict(face_chip)
 #     nromal_embedings = l2_normalize(embedings)
-    
+
 #     for all in database:
 #         list = create list for all face with probability and id
 #     return list
 
 
 class WatchDog():
-
     def __init__(self, detector_path, predictor_path, recognizer_path):
-        self.detector = dlib.cnn_face_detection_model_v1(detector_path) #dlib.get_frontal_face_detector()
+        self.detector = dlib.cnn_face_detection_model_v1(
+            detector_path)  #dlib.get_frontal_face_detector()
         self.predictor = dlib.shape_predictor(predictor_path)
-        self.recognizer = InceptionResNetV1(weights_path=recognizer_path)#dlib.face_recognition_model_v1(face_rec_model_path)
+        self.recognizer = InceptionResNetV1(
+            weights_path=recognizer_path
+        )  #dlib.face_recognition_model_v1(face_rec_model_path)
 
-
-    def identify(self, image): # TODO : images
+    def identify(self, image):  # TODO : images
         detected_faces = self.detector(image)
-        if len(detected_faces)<1:
+        if len(detected_faces) < 1:
             return None
 
         face_points = dlib.full_object_detections()
         for i, person in enumerate(detected_faces):
             face_points.append(self.predictor(image, person.rect))
 
-        
-        face_chips = np.array(dlib.get_face_chips(image, face_points, 160, 0.25))
+        face_chips = np.array(
+            dlib.get_face_chips(image, face_points, 160, 0.25))
         # print(np.array(face_chips).shape)
         # if len(np.array(face_chips).shape)<4:
         #     face_chips = np.expand_dims(np.array(face_chips),axis=0)
         embeddings = self.recognizer.predict(prewhiten(face_chips))
-        
+
         embeddings = l2_normalize(embeddings)
 
-        result = {'DetectedFaces': detected_faces,
-                  'FacePoints' : face_points,
-                  'FaceChips' : face_chips,
-                  'RecognitionID' : embeddings}
+        result = {
+            'DetectedFaces': detected_faces,
+            'FacePoints': face_points,
+            'FaceChips': face_chips,
+            'RecognitionID': embeddings
+        }
 
         return result
+
 
 # shape = self.dog.predictor(frame, subject)
 #############
 # face_chip = dlib.get_face_chip(frame, shape,150,0.25)
-# face_descriptor_from_prealigned_image = self.facerec.compute_face_descriptor(face_chip, 10)  
+# face_descriptor_from_prealigned_image = self.facerec.compute_face_descriptor(face_chip, 10)
 ############
 # face_descriptor_from_prealigned_image = self.facerec.compute_face_descriptor(frame, shape, 10, 0.25)
 ############
