65382f447a727d07be676d45bf853f1972732ce3
Amin <m.amin.kashi@gmail.com>
2019-08-20 15:19:55 +0430

    Multi-CPU, Multi-GPU capability added

--- face.py
+++ face.py
@@ -1,12 +1,12 @@
+import os
+import queue
+import time
+from multiprocessing import JoinableQueue, Process
+
 from kivy.app import App
-# from skimage.io import imsave
 from kivy.clock import Clock
-# from kivy.uix.widget import Widget
-# import time
 from kivy.config import Config
 from kivy.uix.boxlayout import BoxLayout
-# import pandas as pd
-# import numpy as np
 from kivy.uix.recycleview import RecycleView
 
 from utils.databasetools import DataBase
@@ -18,7 +18,6 @@ Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 Config.set('kivy', 'window_icon', './images/facelogo010-01.png')
 Config.set('graphics', 'minimum_width', 425)
 Config.set('graphics', 'minimum_height', 240)
-
 
 DETECTOR_PATH = "./utils/core/mmod_human_face_detector.dat"
 PREDICTOR_PATH = "./utils/core/shape_predictor_5_face_landmarks.dat"
@@ -33,9 +32,6 @@ class RV(RecycleView):
         self.data = [{'text': str(x)} for x in range(100)]
 
 
-# class DataCard(RelativeLayout):
-
-
 class CameraClick(BoxLayout):
     def capture(self):
         '''
@@ -48,15 +44,58 @@ class CameraClick(BoxLayout):
         print("Captured")
 
 
+def f(face_detector_path, predictor_path, keras_weight, frame_queue,
+      result_queue, num, device_id):
+
+    dog = WatchDog(detector_path=face_detector_path,
+                   predictor_path=predictor_path,
+                   recognizer_path=keras_weight,
+                   device_id=device_id)
+
+    while True:
+        try:
+            frame = frame_queue.get_nowait()
+            print(num, "Received frame at ", time.time())
+            if frame is False:
+                break
+            else:
+                start_time = time.time()
+                result_list = dog.identify(frame)
+                print(" ||||||| Process time of  ", num, "  :  ",
+                      time.time() - start_time)
+                print(" ======= End time of  ", num, "  :  ", time.time())
+                result_queue.put([result_list, frame])
+        except queue.Empty:
+            # pass
+            print("######## No frame for", num)
+
+
 class ZharfaApp(App):
+    def __init__(self):
+        super().__init__()
+        self.cm = None
+        self.database = None
+        self.dog = None
+        self.input_image = None
+
+        self.num_dog = 1
+
+        self.frame_queues = [JoinableQueue() for _ in range(self.num_dog)]
+        self.result_queues = [JoinableQueue() for _ in range(self.num_dog)]
+        self.dogs = [
+            Process(target=f,
+                    args=(DETECTOR_PATH, PREDICTOR_PATH, RECOGNIZER_PATH,
+                          self.frame_queues[i], self.result_queues[i], i, i))
+            for i in range(self.num_dog)
+        ]
+
+        self.timer = time.time()
+        self.flag = 0
+
     def build(self):
         self.cm = CameraClick()
-        self.db = DataBase(DATABASE_ADDRESS)
+        self.database = DataBase(DATABASE_ADDRESS)
         Clock.schedule_interval(self.update, 1.0 / 33.0)
-
-        self.dog = WatchDog(detector_path=DETECTOR_PATH,
-                            predictor_path=PREDICTOR_PATH,
-                            recognizer_path=RECOGNIZER_PATH)
 
         self.input_image = InputImage('images/facelogo010-01.png',
                                       'images/Demo1.mkv', 'images/Demo2.mp4')
@@ -66,56 +105,93 @@ class ZharfaApp(App):
                                        width=1920,
                                        hight=1080)
 
+        for i in range(self.num_dog):
+            self.dogs[i].start()
+
         return self.cm
 
-    # def show_people_info(self, result_list):
-    #     #show info of new people
-    #         # if a person is in the view , reset his removal timer
+    def update_save_database(self, result_list):
+        if result_list is not None:
+            correspondence_dict = self.database.update(result_list)
+            self.database.save_data_frame()
+            return correspondence_dict
 
-    #     # remove info of old people
+    def update_frame_viewer(self, result_list, frame, correspondence_dict):
+        if result_list is not None:
+            for i in range(result_list['Size']):
+                rect = result_list['DetectionRects'][i]
+                ###face boxes#################################################
+                if self.cm.ids['face_box'].active:
+                    frame = self.input_image.add_face_boxes(frame, rect)
+                ###show names#################################################
+                if self.cm.ids['names'].active:
+                    id_num = correspondence_dict[i]
+                    text = self.database.dataframe.at[
+                        id_num, 'FirstName'] + ' ' + self.database.dataframe.at[
+                            id_num, 'LastName']
+                    frame = self.input_image.add_name(frame, text, rect)
+                ###show ids#################################################
+                if self.cm.ids['ids'].active:
+                    frame = self.input_image.add_id(
+                        frame, str(correspondence_dict[i]), rect)
+                ##############################################################
+        else:
+            print("No detection at :", time.time())
+
+        print("Screen update at :", time.time())
+        texture = self.input_image.get_kivy_texture(frame)
+        self.cm.ids['camera'].color = (1, 1, 1, 1)
+        self.cm.ids['camera'].texture = texture
 
     def update(self, dt):
         # pass
         if self.cm.ids['play'].state is 'down':
             # 1.capture a frame
-            ret, frame = self.input_image.get_frame()
+            ret, frame_original = self.input_image.get_frame()
 
             if ret is False:
                 self.cm.ids['play'].state = 'normal'
                 # TODO: show a warning!
-                # self.cm.ids['camera'].source = './images/facelogo09-01.png'
                 self.cm.ids['camera'].reload()
+
             else:
-                result_list = self.dog.identify(frame)
-                if result_list is not None:
-                    correspondence_dict = self.db.update(result_list)
-                    self.db.save_data_frame()
-                    for i in range(len(result_list['DetectedFaces'])):
-                        rect = result_list['DetectedFaces'][i].rect
-                        ###face boxes#################################################
-                        if self.cm.ids['face_box'].active:
-                            frame = self.input_image.add_face_boxes(
-                                frame, rect)
-                        ###show names#################################################
-                        if self.cm.ids['names'].active:
-                            id_num = correspondence_dict[i]
-                            text = self.db.dataframe.at[
-                                id_num,
-                                'FirstName'] + ' ' + self.db.dataframe.at[
-                                    id_num, 'LastName']
+                if time.time() - self.timer > 0.200:
+                    self.timer = time.time()
+                    # try:
+                    #     self.frame_queues[self.flag].put_nowait(frame)
+                    # except queue.Full:
+                    #     print(self.flag, " didn't get frame")
+                    if self.frame_queues[self.flag].empty():
+                        self.frame_queues[self.flag].put_nowait(frame_original)
+                        print("sent to ", self.flag, " at ", time.time())
+                    else:
+                        pass
+                        # print(self.flag, " didn't get frame")
+                    self.flag = self.flag + 1
+                    if self.flag == self.num_dog:
+                        self.flag = 0
 
-                            frame = self.input_image.add_name(
-                                frame, text, rect)
-                        ###show ids#################################################
-                        if self.cm.ids['ids'].active:
-                            id_num = correspondence_dict[i]
-                            frame = self.input_image.add_id(
-                                frame, str(id_num), rect)
-                        ##############################################################
+            for i in range(self.num_dog):
+                try:
+                    result_list, frame = self.result_queues[i].get_nowait()
+                    print("received from ", i, " at ", time.time())
+                    correspondence_dict = self.update_save_database(
+                        result_list)
+                    self.update_frame_viewer(result_list, frame,
+                                             correspondence_dict)
+                except queue.Empty:
+                    print("Not received any things at ", time.time())
 
-                texture = self.input_image.get_kivy_texture(frame)
-                self.cm.ids['camera'].color = (1, 1, 1, 1)
-                self.cm.ids['camera'].texture = texture
+    def on_stop(self):
+        for i in range(self.num_dog):
+            self.frame_queues[i].put_nowait(False)
+        time.sleep(1)
+        print('after sleep')
+        for dog in self.dogs:
+
+            dog.terminate()
+            dog.join()
+            dog.close()
 
 
 if __name__ == '__main__':
