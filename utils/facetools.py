bef95bf99f76ebcf41e1eceb9c292b6b10089730
Amin <m.amin.kashi@gmail.com>
2019-09-25 15:27:47 +0330

    Update facetools.py

--- utils/facetools.py
+++ utils/facetools.py
@@ -1,10 +1,13 @@
 import time
-import numpy as np
-import dlib
-import pandas as pd
-from utils.inception_resnet_v1 import InceptionResNetV1
 from multiprocessing import JoinableQueue, Process
+
+import dlib
+import numpy as np
+import pandas as pd
+
 from utils.advanced_queues import AdvancedQueue
+from utils.inception_resnet_v1 import InceptionResNetV1
+from utils.chronometer import Chronometer
 
 pd.set_option('mode.chained_assignment', 'raise')
 
@@ -91,6 +94,7 @@ class DetectionProcess(Process):
         self.input_q = input_queue
         self.output_q = AdvancedQueue(1)
         self.stop_signal_q = JoinableQueue(1)
+        self.chronometer = Chronometer()
 
     def run(self):
         dlib.cuda.set_device(self.device_id)
@@ -103,12 +107,12 @@ class DetectionProcess(Process):
             ################################################################
             usable, item = self.input_q.empty_and_get()
             if usable:
-                print("22222222222222222222 Det starts at: ", time.time())
+                self.chronometer.start()
+                print("============= DetectionProcess: ", time.time())
                 self.input_q.task_done()
                 frame = item
                 if frame is None:
-                    self.output_q.empty_and_put(None)
-                    print("33333333333333333333 Det finishes at: ", time.time(), "NONE")
+                    detection_result = None
                 else:
                     detection_result = detector(frame)
                     detection_confidences = []
@@ -123,9 +127,78 @@ class DetectionProcess(Process):
                         detection_result = {"detection_confidences":detection_confidences,
                                             "detection_rects":detection_rects}
                                             
-                    self.output_q.empty_and_put([frame, detection_result])
-                    print("33333333333333333333 Det finishes at: ", time.time())
+                self.output_q.empty_and_put([frame, detection_result])
+                print("D============ DetectionProcess: ", self.chronometer.average_elapsed())
+                self.chronometer.start()
                 self.output_q.join()
+                print("D============: ", self.chronometer.give_elapsed())
+                
+
+    def end_process(self):
+        self.stop_signal_q.put_nowait(True)
+        time.sleep(1)  #TODO : change duration or remove!
+        self.terminate()
+        self.join()
+        self.close()
+
+
+class FaceEditionProcess(Process):
+    """ Makes the necessary edits for face recognition:
+    1. Predicts human face pose and identifies the location of important facial
+        land marks (such as the corners of the mouth and eyes, tip of the nose)
+    2. Crops faces, rotates them upright and scales them to 160x160 pixels
+    """
+    def __init__(self, predictor_path, device_id, input_queue):
+        super().__init__()
+        self.predictor_path = predictor_path
+        self.device_id = device_id
+        self.input_q = input_queue
+        self.output_q = AdvancedQueue(1)
+        self.stop_signal_q = JoinableQueue(1)
+        self.chronometer = Chronometer()
+
+    def run(self):
+        """ Method representing the process’s activity.
+        """
+        dlib.cuda.set_device(self.device_id)
+        predictor = dlib.shape_predictor(self.predictor_path)
+        while True:
+            ################################################################
+            if self.stop_signal_q.empty() is False:
+                break
+
+            ################################################################
+            usable, item = self.input_q.empty_and_get()
+            if usable:
+                self.chronometer.start()
+                print("############# FaceCorrectionProcess: ", time.time())
+                self.input_q.task_done()
+                frame, detection_result = item
+                if detection_result is None:
+                    result = None
+                else:
+                    
+                    face_points = dlib.full_object_detections()
+                    detection_confidences = detection_result["detection_confidences"]
+                    detection_rects = detection_result["detection_rects"]
+                    for _, rect in enumerate(detection_rects):
+                        face_points.append(predictor(frame, rect))
+                    face_chips = np.array(dlib.get_face_chips(frame, face_points, 160, 0.25))
+                    prewhiten_face_chips = prewhiten(face_chips)
+
+                    result = {
+                        'Size': len(detection_confidences),
+                        'DetectionConfidences': detection_confidences,
+                        'DetectionRects': detection_rects,
+                        'FacePoints': face_points,
+                        'FaceChips': face_chips,
+                        'prewhiten_face_chips': prewhiten_face_chips
+                    }
+                self.output_q.empty_and_put([frame, result])
+                print("D############ FaceCorrectionProcess: ", self.chronometer.average_elapsed())
+                self.chronometer.start()
+                self.output_q.join()
+                print("D############: ", self.chronometer.give_elapsed())
 
     def end_process(self):
         self.stop_signal_q.put_nowait(True)
@@ -136,18 +209,16 @@ class DetectionProcess(Process):
 
 
 class IdentificationProcess(Process):
-    def __init__(self, predictor_path, recognizer_path, device_id, input_queue):
+    def __init__(self, recognizer_path, device_id, input_queue):
         super().__init__()
-        self.predictor_path = predictor_path
         self.recognizer_path = recognizer_path
         self.device_id = device_id
         self.input_q = input_queue
         self.output_q = AdvancedQueue(1)
         self.stop_signal_q = JoinableQueue(1)
+        self.chronometer = Chronometer()
 
     def run(self):
-        dlib.cuda.set_device(self.device_id)
-        predictor = dlib.shape_predictor(self.predictor_path)
         recognizer = InceptionResNetV1(weights_path=self.recognizer_path, device_id=str(self.device_id))
         while True:
             ################################################################
@@ -157,34 +228,21 @@ class IdentificationProcess(Process):
             ################################################################
             usable, item = self.input_q.empty_and_get()
             if usable:
-                print("44444444444444444444 Iden starts at: ", time.time())
+                self.chronometer.start()
+                print("+++++++++++++IdentificationProcess: ", time.time())
                 self.input_q.task_done()
-                if item is None:
-                    self.output_q.empty_and_put(None)
-                    print("5555555555555555555 Iden finishes at: ", time.time(), "NONEEEE")
+                frame, edited_result = item
+                if edited_result is None:
+                    result = None
                 else:
-                    frame, detection_result = item
-                    if detection_result is None:
-                        result = None
-                    else:
-                        face_points = dlib.full_object_detections()
-                        detection_confidences = detection_result["detection_confidences"]
-                        detection_rects = detection_result["detection_rects"]
-                        for _, rect in enumerate(detection_rects):
-                            face_points.append(predictor(frame, rect))
-                        face_chips = np.array(dlib.get_face_chips(frame, face_points, 160, 0.25))
-                        embeddings = recognizer.predict(prewhiten(face_chips))
-                        embeddings = l2_normalize(embeddings)
-                        result = {
-                            'Size': len(detection_confidences),
-                            'DetectionConfidences': detection_confidences,
-                            'DetectionRects': detection_rects,
-                            'FacePoints': face_points,
-                            'FaceChips': face_chips,
-                            'RecognitionID': embeddings
-                        }
-                    self.output_q.empty_and_put([frame, result])
-                    print("5555555555555555555 Iden finishes at: ", time.time())
+                    prewhiten_face_chips = edited_result["prewhiten_face_chips"]
+                    embeddings = recognizer.predict(prewhiten_face_chips)
+                    embeddings = l2_normalize(embeddings)
+                    edited_result.update({'RecognitionID': embeddings})
+                    del edited_result["prewhiten_face_chips"]
+                    result = edited_result
+                print("D++++++++++++IdentificationProcess: ", self.chronometer.average_elapsed())
+                self.output_q.empty_and_put([frame, result])
 
     def end_process(self):
         self.stop_signal_q.put_nowait(True)
